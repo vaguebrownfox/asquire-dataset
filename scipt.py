5799aa876166dac4a7e9e5b322fe612361081cd8
Jeevan Kylash <foxjournal.nsfw@gmail.com>
2021-09-20 21:09:53 +0530

    messages

--- scipt.py
+++ scipt.py
@@ -11,29 +11,24 @@ unpack_folder = "data/unpackd_audio"
 audio_files = os.listdir(audio_folder)
 pad = 0.7;
 
-def rmNums(t) :
+def rm_nums(t) :
     return ''.join([c for c in t if not c.isdigit()]).strip()
 
-def unpackWav(audio, anote, filename):
-    fs, data = wavfile.read(audio)
-    channel1 = data[:, 0]
-    
-    print(filename, "pd read")
-    
+def get_anote_frames(anoteFile) :
     anote_frame = pd.read_csv(
-        anote, sep='\t', 
+        anoteFile, sep='\t', 
         header=None, 
         usecols=[0, 1, 2], 
         names=['start', 'end', 'type'])
     
     anote_frame.insert(0, 'index', range(0, len(anote_frame)))
     
-    # print(filename, anote_frame['type'].unique())
-    
-    cleanTypesDigits = [rmNums(t) for t in anote_frame['type'].unique() if isinstance(t, str)]
-    types = list(pd.DataFrame({'type': cleanTypesDigits})['type'].unique())
-    
-    # print(filename, "types", types)
+    return anote_frame
+
+def get_types(anote_frame) :
+    clean_types_digits = [rm_nums(t) for t in anote_frame['type']
+                        .unique() if isinstance(t, str)]
+    types = list(pd.DataFrame({'type': clean_types_digits})['type'].unique())
     
     # merge types
     m_types = ["Exhale", "Wheeze", "Inhale"]
@@ -44,46 +39,45 @@ def unpackWav(audio, anote, filename):
             continue
     types.append("|".join(m_types))
     
-    unpack_info = {}
+    return types
+    
+    
+def unpack_wav(audio, anote, filename):
+    fs, data = wavfile.read(audio)
+    channel_1 = data[:, 0]
+    
+    anote_frame = get_anote_frames(anote)
+
+    types = get_types(anote_frame)
+    
     for t in types :
-       
-        type_frame = anote_frame[anote_frame['type'].str.contains(t, na=False)]
+        type_frame = anote_frame[anote_frame['type']
+                                 .str.contains(t, na=False)]
         index = type_frame["index"].to_numpy()
         
-        pos = []
-        bp_arr = []
         bp = 0
+        bp_arr = []
+        pos = []
         pos.append((type_frame["start"][index[0]]- pad) * fs)
         for i in range(len(index) - 1):
             if index[i + 1] - index[i] > 1 :
                 pos.append((type_frame["end"][index[i]] + pad) * fs)
-                pos.append((type_frame["start"][index[i+1]] - pad) * fs)
+                pos.append((type_frame["start"][index[i + 1]] - pad) * fs)
                 bp = i + 1 - bp
                 bp_arr.append(bp);
         bp_arr.append(i + 2 - bp)
         pos.append((type_frame["end"][index[len(index) - 1]] + pad) * fs)
         
         pos = list(map(int, pos))
-
         for i in range(int(len(pos) / 2)):
             ptr = i * 2
-            chunk = channel1[pos[ptr] : pos[ptr + 1]]
+            chunk = channel_1[pos[ptr] : pos[ptr + 1]]
             filename = ''.join(filename.split("_")[0])
-            unpack_file = os.path.join(f"{unpack_folder}", 
+            unpack_file = os.path.join(f"{unpack_folder}",
                                        f"{filename}_{t}_{i+1}_{bp_arr[i]}.wav")
             wavfile.write(unpack_file, fs, chunk)
-        
-
-
 
 for file in audio_files:
     audiopath = os.path.join(audio_folder, file)
     anotepath = os.path.join(anote_folder, file.replace("wav", "txt"))
-    unpackWav(audiopath, anotepath, file.replace(".wav", ""))
-
-    
-
-
-    
-    
-    
+    unpack_wav(audiopath, anotepath, file.replace(".wav", ""))
